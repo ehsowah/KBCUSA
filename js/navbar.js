c630607c217858bf40d77a0ba9ebe6ce5963c131
ehsowah <56480539+ehsowah@users.noreply.github.com>
2022-10-06 21:11:05 -0400

    adding about, dropdown, history page, add home on logo

--- js/navbar.js
+++ js/navbar.js
@@ -3,6 +3,10 @@ toggle between hiding and showing the dropdown content */
 function myNavFunction() {
     document.getElementById("myDropdown").classList.toggle("show");
   }
+
+  function myNavFunction2() {
+    document.getElementById("myDropdown2").classList.toggle("show");
+  }
   
   // Close the dropdown if the user clicks outside of it
   window.onclick = function(e) {
@@ -11,5 +15,13 @@ function myNavFunction() {
       if (myDropdown.classList.contains('show')) {
         myDropdown.classList.remove('show');
       }
+
+      var myDropdown2 = document.getElementById("myDropdown2");
+    if (myDropdown2.classList.contains('show')) {
+      myDropdown2.classList.remove('show');
     }
+    }
+
+    
+  
   }
